c0c1b3cfaf4fa8e0f8d49f57b1044b9fbf71ac66
RonanDrouglazet <ronan.drouglazet@teads.tv>
2019-10-25 22:05:32 +0200

    update

--- js/site.js
+++ js/site.js
@@ -1,11 +1,11 @@
-const setButtonLineRect = rect => $('.menu .button-line').css({
+const setButtonLineRect = rect => $('header .menu .button-line').css({
     left: rect.left,
     width: rect.width,
     top: rect.top + rect.height
 })
 
 const getMenuButtonFromSection = section =>
-    $(`.menu .button[data-menu="${$(section).data('menu')}"]`).get(0)
+    $(`header .menu .button[data-menu="${$(section).data('menu')}"]`).get(0)
 
 let currentPosition
 let timeoutPosition
@@ -22,7 +22,7 @@ const initMenu = () => {
     document.querySelectorAll('section').forEach(section => observer.observe(section))
     observer.observe(document.querySelector('footer'))
 
-    $('.menu .button').hover(function() {
+    $('header .menu .button').hover(function() {
         clearTimeout(timeoutPosition)
         setButtonLineRect(this.getBoundingClientRect())
     }, () => { timeoutPosition = setTimeout(() => setButtonLineRect(currentPosition()), 500) })
@@ -33,6 +33,36 @@ const initMenu = () => {
     })
 }
 
+const initMore = () => {
+    $('.learn-more, .read-more').each(function() {
+        const button = $(this)
+        const sub = button.parent().find('.sub-content')
+        const height = () => sub.find('.description.active').height()
+
+        button.click(() => sub.css('height', sub.height() ? 0 : height() + 'px'))
+    })
+}
+
+const initSub = () => {
+    $('.sub-content .menu button:not(.title').each(function() {
+        $(this).click(function() {
+            const toShow = $('.sub-content .description.' + $(this).data('menu'))
+            const current = $('.sub-content .description.active')
+            const height = Math.max(toShow.height(), $('.sub-content .menu').height())
+
+            if (toShow !== current) {
+                current.toggleClass('active', 'out')
+                toShow.toggleClass('active')
+                $('.sub-content .menu button.active').toggleClass('active')
+                $(this).toggleClass('active')
+                $('.sub-content').css('height', height + 'px')
+            }
+        })
+    })
+}
+
 $(window).ready(() => {
     initMenu()
+    initMore()
+    initSub()
 })
